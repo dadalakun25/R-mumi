bd45fc36dde9232f77075e81a15d739569e50c41
b06505002 <32170193+dadalakun25@users.noreply.github.com>
2018-09-19 20:21:14 +0800

    作業更新

--- week_2/hw_2/OOXX_game.R
+++ week_2/hw_2/OOXX_game.R
@@ -1,42 +1,95 @@
 p1_f <- 0
 p2_f <- 0
 count <- 1
-ooxx1 <- c(" ","|"," ","|"," ")
-ooxx1.5 <- c("一","一","一","一")
-ooxx2 <- c(" ","|"," ","|"," ")
-ooxx2.5 <- c("一","一","一","一")
-ooxx3 <- c(" ","|"," ","|"," ")
-print("ゲームスタート！")
-cat(ooxx1)
-cat(ooxx1.5)
-cat(ooxx2)
-cat(ooxx2.5)
-cat(ooxx3)
-while(p1_f & p2_f ==0){
-  if(count%%2==1){
+ooxx <- c(as.character(seq(from =1, to=9, by=1)))
+print("OOXX遊戲開始")
+while(p1_f==p2_f){
+  if(count==9){
+    print("和局")
+    break
+  }else if(count%%2==1){
     switch(scan(),
-            ooxx1[1] <- "O",
-            ooxx1[3] <- "O",
-            ooxx1[5] <- "O",
-            ooxx2[1] <- "O",
-            ooxx2[3] <- "O",
-            ooxx2[5] <- "O",
-            ooxx3[1] <- "O",
-            ooxx3[3] <- "O",
-            ooxx3[5] <- "O" )
+           ooxx[1] <- "O",
+           ooxx[2] <- "O",
+           ooxx[3] <- "O",
+           ooxx[4] <- "O",
+           ooxx[5] <- "O",
+           ooxx[6] <- "O",
+           ooxx[7] <- "O",
+           ooxx[8] <- "O",
+           ooxx[9] <- "O" )
   }else{
     switch(scan(),
-           ooxx1[1] <- "X",
-           ooxx1[3] <- "X",
-           ooxx1[5] <- "X",
-           ooxx2[1] <- "X",
-           ooxx2[3] <- "X",
-           ooxx2[5] <- "X",
-           ooxx3[1] <- "X",
-           ooxx3[3] <- "X",
-           ooxx3[5] <- "X" )
+           ooxx[1] <- "X",
+           ooxx[2] <- "X",
+           ooxx[3] <- "X",
+           ooxx[4] <- "X",
+           ooxx[5] <- "X",
+           ooxx[6] <- "X",
+           ooxx[7] <- "X",
+           ooxx[8] <- "X",
+           ooxx[9] <- "X" )
   }
-  
-  count <- count + 1
+  cat(ooxx[1],"|",ooxx[2],"|",ooxx[3])
+  cat("\n一","一","一","一\n")
+  cat(ooxx[4],"|",ooxx[5],"|",ooxx[6])
+  cat("\n一","一","一","一\n")
+  cat(ooxx[7],"|",ooxx[8],"|",ooxx[9])
+  if(count%%2==1){
+    if(ooxx[1]==ooxx[2]){
+      if(ooxx[2]==ooxx[3]) p1_f <- 1  
+    }
+    if(ooxx[4]==ooxx[5]){
+      if(ooxx[5]==ooxx[6]) p1_f <- 1  
+    }
+    if(ooxx[7]==ooxx[8]){
+      if(ooxx[8]==ooxx[9]) p1_f <- 1  
+    }
+    if(ooxx[1]==ooxx[4]){
+      if(ooxx[4]==ooxx[7]) p1_f <- 1  
+    }
+    if(ooxx[2]==ooxx[5]){
+      if(ooxx[5]==ooxx[8]) p1_f <- 1  
+    }
+    if(ooxx[3]==ooxx[6]){
+      if(ooxx[6]==ooxx[9]) p1_f <- 1  
+    }
+    if(ooxx[1]==ooxx[5]){
+      if(ooxx[5]==ooxx[9]) p1_f <- 1  
+    }
+    if(ooxx[3]==ooxx[5]){
+      if(ooxx[5]==ooxx[7]) p1_f <- 1  
+    }
+  }else{
+    if(ooxx[1]==ooxx[2]){
+      if(ooxx[2]==ooxx[3]) p2_f <- 1  
+    }
+    if(ooxx[4]==ooxx[5]){
+      if(ooxx[5]==ooxx[6]) p2_f <- 1  
+    }
+    if(ooxx[7]==ooxx[8]){
+      if(ooxx[8]==ooxx[9]) p2_f <- 1  
+    }
+    if(ooxx[1]==ooxx[4]){
+      if(ooxx[4]==ooxx[7]) p2_f <- 1  
+    }
+    if(ooxx[2]==ooxx[5]){
+      if(ooxx[5]==ooxx[8]) p2_f <- 1  
+    }
+    if(ooxx[3]==ooxx[6]){
+      if(ooxx[6]==ooxx[9]) p2_f <- 1  
+    }
+    if(ooxx[1]==ooxx[5]){
+      if(ooxx[5]==ooxx[9]) p2_f <- 1  
+    }
+    if(ooxx[3]==ooxx[5]){
+      if(ooxx[5]==ooxx[7]) p2_f <- 1  
+    }
+  }
+  count <- count + 1  
 }
+
+if(p1_f==1){
+  print("玩家一獲勝!")
+} else if(p2_f==1) print("玩家二獲勝!")
 
